de236fe902969e6af4f99d917a719ea64860e022
Salma Shaik <salmashaik1611@gmail.com>
2017-03-24 17:39:31 +0530

    refactored to use only hashmap

--- COP_Pract/ProgCoreJava/DuplicateArrayElementFinder.java
+++ COP_Pract/ProgCoreJava/DuplicateArrayElementFinder.java
@@ -5,25 +5,22 @@ import java.util.HashMap;
 public class DuplicateArrayElementFinder{
 	public static void main(String[] args){		
 		int[] intArray = {24,24,6,3,6,8,3,2,6,6,856,3};
-		List<Integer> dupList = new ArrayList<Integer>();
 		Map<Integer,Integer> dupMap = new HashMap<>();
 		for(int i=0;i<intArray.length;i++){
-			if (!dupList.contains(intArray[i])){
-			for(int j=i+1;j<intArray.length;j++){
-				if(intArray[j]==intArray[i]){
-					int dupCount=0;		
-					dupList.add(intArray[i]);
-					dupMap.put(intArray[i],++dupCount);
-					break;
-				}
-			 }
-		    }else{ 
-					int value = dupMap.get(intArray[i]);
-					dupMap.put(intArray[i], ++value);
-				}
+			if (!dupMap.containsKey(intArray[i])){
+				int dupCount=1;	
+				for(int j=i+1;j<intArray.length;j++){
+						if(intArray[j]==intArray[i]){
+						dupCount++;
+					}
+					if(dupCount>1){
+						dupMap.put(intArray[i],dupCount);
+					}
+				 }
+		    }
 		}
 		for(Integer key : dupMap.keySet()){
-			System.out.println("Dup Number: " +key+ " ,Count: " +dupMap.get(key));
+			System.out.println("Dup Number: " +key+ " ,Number of Occurences: " +dupMap.get(key));
 		}
 	}
 }
